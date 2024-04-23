0eed9001ae24491f5b35ab577fac2ec7b6762464
charlieolcott <113263130+charlieolcott@users.noreply.github.com>
2024-04-23 00:02:35 -0400

    feat: added a quick ugly placeholder to visualize how sessions will work for profiles

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,9 +1,16 @@
 interface ProfileProps {
-  props: any; // placeholder
+  props: any; // Placeholder, replace or remove as needed
 }
 
 const Profile = ({ props }: ProfileProps) => {
-  return <div id="profile">Profile included here!</div>;
+  return (
+    <div id="profile">
+      <div className="profile-title">Session on 04/21/2024 at 6:00pm</div>
+      <div className="profile-song">Song 1</div>
+      <div className="profile-song">Song 2</div>
+      <div className="profile-song">Song 3</div>
+    </div>
+  );
 };
 
 export default Profile;
