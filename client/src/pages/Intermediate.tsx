32cc83ca969414542e32bbb2e06899642bd956cd
jennliao <jennifer_liao@brown.edu>
2024-04-27 18:53:05 -0400

    feat: get playlist implemented, need to double check display

--- client/src/pages/Intermediate.tsx
+++ client/src/pages/Intermediate.tsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { PageProps } from '../interfaces/interfaces';
-import { removeLoginCookie } from '../utils/cookie';
-import AccountLogin from './AccountLogin';
+import React, { useState, useEffect } from "react";
+import { PageProps } from "../interfaces/interfaces";
+import { removeLoginCookie } from "../utils/cookie";
+import AccountLogin from "./AccountLogin";
+import axios from "axios";
+
+const PLAYLIST_ENDPOINT = "https://api.spotify.com/v1/me/playlists";
 
 interface IntermediateProps {
   pageProps: PageProps;
@@ -9,6 +12,37 @@ interface IntermediateProps {
 }
 
 const Intermediate = ({ pageProps, setIsAuthenticated }: IntermediateProps) => {
+  const [token, setToken] = useState("");
+  const [data, setData] = useState<JSON>();
+  const [playlists, setPlaylists] = useState([]);
+
+  useEffect(() => {
+    const retrievedToken = localStorage.getItem("accessToken");
+    if (retrievedToken != null) {
+      setToken(retrievedToken);
+    }
+    getPlaylists();
+  }, []);
+
+  const getPlaylists = () => {
+    console.log("token: " + token);
+
+    axios
+      .get(PLAYLIST_ENDPOINT, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+        setPlaylists(response.data.items);
+        console.log(response.data.items);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   const { setPage } = pageProps;
   const [signedInWithSpotify, setSignedInWithSpotify] = useState(false);
 
@@ -34,22 +68,54 @@ const Intermediate = ({ pageProps, setIsAuthenticated }: IntermediateProps) => {
 
   if (signedInWithSpotify) {
     return (
-      <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <h1>Select a Playlist</h1>
-        <button onClick={() => console.log("Playlist One selected")}>Playlist One</button>
-        <button onClick={() => console.log("Playlist Two selected")}>Playlist Two</button>
-        <button onClick={() => console.log("Playlist Three selected")}>Playlist Three</button>
+
+        {playlists.map((playlist: { name: string }) => (
+          <p>{playlist.name}</p>
+        ))}
+
+        <button onClick={() => console.log("Playlist One selected")}>
+          Playlist One
+        </button>
+        <button onClick={() => console.log("Playlist Two selected")}>
+          Playlist Two
+        </button>
+        <button onClick={() => console.log("Playlist Three selected")}>
+          Playlist Three
+        </button>
         <button onClick={handleContinue}>Continue</button>
-        <button onClick={handleLogout} style={{ marginTop: '20px' }}>Logout</button>
+        <button onClick={handleLogout} style={{ marginTop: "20px" }}>
+          Logout
+        </button>
       </div>
     );
   }
 
   return (
-    <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <AccountLogin onLoginSuccess={handleSpotifyLoginSuccess} />
-      <button onClick={() => setPage("music")}>Continue without signing into Spotify</button>
-      <button onClick={handleLogout} style={{ marginLeft: '20px' }}>Logout</button>
+      <button onClick={() => setPage("music")}>
+        Continue without signing into Spotify
+      </button>
+      <button onClick={handleLogout} style={{ marginLeft: "20px" }}>
+        Logout
+      </button>
     </div>
   );
 };
