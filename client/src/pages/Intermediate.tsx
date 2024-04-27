2484038a11b0d1b7c74dc706286071aa0eb6dc83
charlieolcott <113263130+charlieolcott@users.noreply.github.com>
2024-04-27 18:25:56 -0400

    feat: changed playlist selection to a radio button

--- client/src/pages/Intermediate.tsx
+++ client/src/pages/Intermediate.tsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { PageProps } from '../interfaces/interfaces';
 import { removeLoginCookie } from '../utils/cookie';
 import AccountLogin from './AccountLogin';
+import signOutSpotify from './AccountLogin';
 
 interface IntermediateProps {
   pageProps: PageProps;
   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
 
 const Intermediate = ({ pageProps, setIsAuthenticated }: IntermediateProps) => {
   const { setPage } = pageProps;
@@ -36,9 +38,14 @@ const Intermediate = ({ pageProps, setIsAuthenticated }: IntermediateProps) => {
     return (
       <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
         <h1>Select a Playlist</h1>
-        <button onClick={() => console.log("Playlist One selected")}>Playlist One</button>
-        <button onClick={() => console.log("Playlist Two selected")}>Playlist Two</button>
-        <button onClick={() => console.log("Playlist Three selected")}>Playlist Three</button>
+        <div className="radio-group">
+          <input type="radio" id="playlist1" name="playlist" value="playlist1"></input>
+          <label htmlFor="playlist1">Playlist One</label>
+          <input type="radio" id="playlist2" name="playlist" value="playlist2"></input>
+          <label htmlFor="playlist2">Playlist Two</label>
+          <input type="radio" id="playlist3" name="playlist" value="playlist3"></input>
+          <label htmlFor="playlist3">Playlist Three</label>
+        </div>
         <button onClick={handleContinue}>Continue</button>
         <button onClick={handleLogout} style={{ marginTop: '20px' }}>Logout</button>
       </div>
